44f34e16e15da1684342fc22919b1301129b9a8d
G8t0rboi <39623865+G8t0rboi@users.noreply.github.com>
2025-03-10 03:27:45 -0400

    Add back button to home page

--- src/app/photobooth/page.js
+++ src/app/photobooth/page.js
@@ -1,8 +1,21 @@
 import Camera from "../components/camera";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-  return (
-    <div className="bg-(--primary-light) text-(--primary-dark) h-screen w-screen flex flex-col items-center justify-center">
-      <Camera />
-    </div>
-  );
+	return (
+		<div className="bg-(--primary-light) text-(--primary-dark) h-screen w-screen flex flex-col items-center justify-center">
+			<Link href="/">
+				<Image
+					src="/back_arrow.svg"
+					alt="Back arrow"
+					width={50}
+					height={50}
+                    className="absolute top-4 left-4 hover:scale-110 transition ease-in-out duration-300"
+				/>
+			</Link>
+
+			<Camera />
+		</div>
+	);
 }
