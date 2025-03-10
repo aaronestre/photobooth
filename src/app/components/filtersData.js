3c696662807f6750be7af83dc7d41b64cadc4a44
G8t0rboi <39623865+G8t0rboi@users.noreply.github.com>
2025-03-10 01:56:59 -0400

    Update greyscale filter to be more contrasty

--- src/app/components/filtersData.js
+++ src/app/components/filtersData.js
@@ -1,6 +1,6 @@
 export const FILTERS = [
     { name: "Normal", value: "" },
-    { name: "Grayscale", value: "grayscale(100%)" },
+    { name: "Black and White", value: "grayscale(100%) contrast(105%)" },
     {name: "Summer Heat", value: "brightness(103%) contrast(114%) grayscale(0%) hue-rotate(0deg) invert(0%) opacity(100%) saturate(122%) sepia(0%)"},
     {name: "Vintage", value: "brightness(105%)  grayscale(100%) hue-rotate(0deg) invert(0%) opacity(100%) saturate(100%) sepia(50%)"},
   { name: "Soft Blur", value: "blur(2px) brightness(1.1)" },
